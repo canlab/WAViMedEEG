f14617a0f077ff27498523c82e5bbbb5296e802a
claytonjschneider <claytonjschneider@gmail.com>
2019-06-05 02:05:15 -0600

    Rearranged subfolders

--- waviTensor.py
+++ waviTensor.py
@@ -4,24 +4,62 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import numpy as np
+import csv
 import tensorflow as tf
 import sys, os, re
 
-# Make numpy values easier to read
+# make numpy values easier to read
 np.set_printoptions(precision=3, suppress=True)
 
-filename=sys.argv[1]
-n=sys.argv[2]
+foldername=sys.argv[1]
 
-LABELS=[0,1]
+LABELS = [0,1]
+filenames = os.listdir(foldername)
+n = len(filenames)
 
-# def get_dataset(file_path, numSubjects):
-#     dataset = tf.data.experimental.make_csv_dataset(
-#         file_path,
-#         batch_size=15,
-#         #na_value="NaN",
-#         num_epochs=1,
-#     )
-#     return dataset
+def printCSV(sub):
+    with open(foldername+filenames[sub], newline='') as File:
+        reader = csv.reader(File)
+        for row in reader:
+            print(row)
+
+# def get_model(input_dim, hidden_units=[100]):
+#     """
+#     Create a Keras model with layers
 #
-# raw_train_data = get_dataset(filename, n)
+#     Args:
+#         input_dim: (int) The shape of an item in a batch
+#         labels_dim: (int) The shape of a label
+#         hidden_units: [int] the layer sizes of the DNN (input layer first)
+#         learning_rate: (float) the learning rate for the optimizer
+#
+#     Returns:
+#         A Keras model
+#     """
+#     inputs = tf.keras.Input(shape=(input_dim,))
+#     x = inputs
+#
+#     for units in hidden_units:
+#         x = tf.keras.layers.Dense(units, activation='relu')(x)
+#     outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+#
+#     model = tf.keras.Model(inputs, outputs)
+#
+#     return Model
+#
+# input_shape, output_shape = train_data.output_shapes
+# input_dimension = input_shape.dims[1] # [0] is the batch size
+#
+# model = get_model(input_dimension)
+# model.compile(
+#     loss = 'binary_crossentropy',
+#     optimizer = 'adam',
+#     metrics = ['accuracy']
+# )
+# model.fit(train_data, epochs=20)
+#
+# test_loss, test_accuracy = model.evaluate(test_data)
+#
+# print('\n\nTest Loss {}, Test Accuracy {}'.format(test_loss, test_accuracy))
+#
+# predictions = model.predict(test_data)
