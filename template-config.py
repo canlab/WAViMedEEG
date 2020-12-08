760cee0a4410b2b257d82f9879c0f9f084b5002b
claytonjschneider <claytonjschneider@gmail.com>
2020-12-08 11:48:57 -0700

    copied template and removed deleted files from tracking

--- template-config.py
+++ template-config.py
@@ -1,40 +1,85 @@
-# ===================== WAVi Analysis Toolbox Config Overview ===================== #
-# Welcome to the WAVi EEG Analysis Toolbox. Before running this code, set your      #
-# configuration variables below. Most classes, scripts, etc., will reference it.    #
+"""
+=== WAVi Analysis Toolbox Config Overview ===
 
-# INITIALIZING STUDY FILES
-# ================================================================================= #
-# before beginning with new data, format your new study directory as follows:       #
-# ~/.../path/to/mystudies/newstudydir
-# ----------> /raw
-# --------------------> *.eeg
-# --------------------> *.evt
-# --------------------> *.art
+Welcome to the WAVi EEG Analysis Toolbox.
+Before running this code, set your configuration variables below.
+Most classes, scripts, etc., will reference it.
 
-# ----------------------
-myStudies = "/wavi/EEGstudies" # if you are working with multiple studies at once, set the parent directory where you will keep them all
-studyDirectory = myStudies+"/BrainStudy" # specific study, for functions that only deal with 1 at a time
+INITIALIZING STUDY FILES
+========================
+before beginning with new data, format your new study directory as follows:
+~/.../path/to/mystudies/newstudydir
+----------> /raw
+--------------------> <subjID>_<taskID>.eeg
+--------------------> <subjID>_<taskID>.evt
+--------------------> <subjID>_<taskID>.art
+--------------------> *...
 
-tasks = {
-    "P300": ["p300", "P300_Eyes_Closed", "P300-Sync_Blink", "P300_Eye_Closed", "P300s", "P300-2"],
-    "FLNK": ["Flanker", "flanker"],
-    "CLSD": ["Eyes_Closed_Resting", "CLSD-2"],
-    "OPEN": ["Eyes_Open_Focused"],
-    "REST": ["Rest", "rest"],
-    "CRNC": ["Chronic", "chronic"],
-    "SOMA": ["SMS"]
-}
-
+========================
+"""
 
 # this package expects a naming convention for raw EEG files:
 # n-digit participant number, underscore, task name, .art / .eeg / .evt
 # Ex: 104_p300.eeg
 # if you want to use a different length participant identifier, specify it here
-participantNumLen = 4 # default length
-sampleRate = 250 # in Hz
-artDegree = 2 # highest number of WAVi-supplied artifact
-# still accepted by the program, 0 (strict), 1 (loose), or 2 (none)
+participantNumLen = 4  # default length
 
+# if you are working with multiple studies at once,
+# set the parent directory where you will keep them all
+myStudies = "/wavi/EEGstudies"
+studyDirectory = 'CANlab'
+
+excludeSubs = [
+    '1004',
+    '1020']
+
+group_names = {
+    1: "Control",
+    2: "Pain"
+}
+
+refGroupFolders = [
+    "ref 24-30",
+    "ref 31-40",
+    "ref 41-50",
+    # "ref 51-60",
+    # "ref 61-70",
+    # "ref 71-80",
+    # "ref 81+"
+    ]
+
+# dictionary where key==task names, = 'unclean' task names
+# so that filenames can be cleaned automatically
+tasks = {
+    "P300": [
+        "p300",
+        "P300_Eyes_Closed",
+        "P300-Sync_Blink",
+        "P300_Eye_Closed",
+        "P300s",
+        "P300-2",
+        "P300_EC_4_Min"],
+    "FLNK": [
+        "Flanker",
+        "flanker"],
+    "CLSD": [
+        "Eyes_Closed_Resting",
+        "CLSD-2"],
+    "OPEN": [
+        "Eyes_Open_Focused"],
+    "REST": [
+        "Rest",
+        "rest"],
+    "CRNC": [
+        "Chronic",
+        "chronic"],
+    "SOMA": [
+        "SMS"]}
+
+
+sampleRate = 250  # in Hz
+
+# still accepted by the program, 0 (strict), 1 (loose), or 2 (none)
 
 # default channel names, customize if using non-WAVi headset
 channel_names = [
@@ -56,8 +101,7 @@ channel_names = [
     'T6',
     'Fz',
     'Cz',
-    'Pz'
-]
+    'Pz']
 
 # channels to be used for artifacting, contigification
 # and ultimately used in neural net analyses
@@ -83,8 +127,9 @@ network_channels = [
     'T6',
     'Fz',
     'Cz',
-    'Pz'
-]
+    'Pz']
 
-# kernel type for SVM analysis
-kernel_type = "linear"
+frequency_bands = {
+    "delta": [0, 4],
+    "alpha": [6, 13],
+}
