48298d921d3a539b57a43e62033c779bd834cb45
claytonjschneider <claytonjschneider@gmail.com>
2021-07-02 13:32:21 -0600

    just 1 new batch script

---
+++ batch/jul1.sh
@@ -0,0 +1,3 @@
+python3 ../scripts/Run_cnn.py --study_names WD_31-40 rehab_train --erp_degree 1 --filter_band hidelta --epochs 500 --tt_split 0 --lr_decay True --regularizer_param 0.035 --dropout 0.15
+
+
